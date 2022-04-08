6abc1d29651817d16f0a017041123de10cb097d3
xxxxxlyf <62167879+xxxxxlyf@users.noreply.github.com>
2022-04-08 20:29:22 +0800

    自定义判断是否强制登录注解

--- src/main/java/com/nowcoder/community/annotation/LoginRequired.java
+++ src/main/java/com/nowcoder/community/annotation/LoginRequired.java
@@ -1,16 +1,19 @@
 package com.nowcoder.community.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author lyf
- * @description 登录注解
- * @create 2022-03-27 17:02
- **/
-
+ * @projectName community
+ * @date 2022/4/8 下午 08:00
+ * @description 自定义注解 判断是否是登陆情形下才能够进行页面的访问
+ * 作用在方法上，运行时有效
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface LoginRequired {
-
-    /**
-     * true时，标识当前不需要经过拦截器逻辑拦截
-     * @return
-     */
-    boolean required() default false;
 }
+

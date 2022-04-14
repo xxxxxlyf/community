883b8f386657aadcb6857327353ef54dd9b780aa
xxxxxlyf <62167879+xxxxxlyf@users.noreply.github.com>
2022-04-14 19:22:32 +0800

    查询帖子详情

--- src/main/java/com/nowcoder/community/controller/DiscussPostController.java
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -1,14 +1,16 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.annotation.LoginRequired;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.utils.CommunityUtil;
 import com.nowcoder.community.utils.UserHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author lyf
@@ -16,7 +18,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @create 2022-04-13 22:09
  * 帖子
  **/
-@Controller("/discussPost")
+@Controller
+@RequestMapping("/discussPost")
 public class DiscussPostController {
 
     @Autowired
@@ -24,6 +27,9 @@ public class DiscussPostController {
 
     @Autowired
     private DiscussPostService postService;
+
+    @Autowired
+    private UserService userService;
 
 
     @ResponseBody
@@ -33,12 +39,31 @@ public class DiscussPostController {
         if(postService==null){
             return CommunityUtil.getReturnMsg("您还没有登录哦！",403);
         }else{
-           int i=  postService.addPosts(content,title,u.getId());
-           if(i==1){
-               return CommunityUtil.getReturnMsg("发布成功",0);
-           }else {
-               throw  new RuntimeException("数据执行异常，添加帖子失败");
-           }
+            int i=  postService.addPosts(content,title,u.getId());
+            if(i==1){
+                return CommunityUtil.getReturnMsg("发布成功",0);
+            }else {
+                throw  new RuntimeException("数据执行异常，添加帖子失败");
+            }
         }
+    }
+
+
+    /**
+     * 查询帖子详情[根据帖子id]
+     * @param id
+     * @return
+     */
+    @LoginRequired
+    @GetMapping("/getPostDetail/{id}")
+    public String getPostDetail(@PathVariable("id") int id, Model model){
+        DiscussPost post=postService.getPostById(id);
+        model.addAttribute("post",post);
+        //查詢作者信息
+        User user=userService.selectUserById(post.getUserId());
+        model.addAttribute("user",user);
+
+        //返回帖子詳情的處理頁面
+        return "/site/discuss-detail";
     }
 }
